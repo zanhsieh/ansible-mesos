2be398221717f24bfd35e05c2dc3ce7cad57cfc4
zanhsieh <zanhsieh@gmail.com>
2016-02-19 13:25:03 +0800

    fix mesos_master reference bug

--- init-cluster.sh
+++ init-cluster.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-mesos_master=192.168.122.11
+mesos_master="192.168.122.11"
 
 for i in task-data/marathon/*.json; do
   echo "Deploying ${i} to Marathon"
-  curl -L -H "Content-Type: application/json" -X POST 'http://$mesos_master:8080/v2/apps?force=true' -d@${i} &>/dev/null
+  curl -L -H "Content-Type: application/json" -X POST "http://${mesos_master}:8080/v2/apps?force=true" -d@${i} &> /dev/null
 done
 
 for i in task-data/chronos/*.json; do
   echo "Deploying ${i} to Chronos"
-  curl -L -H "Content-Type: application/json" -X POST 'http://$mesos_master:4400/scheduler/iso8601' -d@${i}  &>/dev/null
+  curl -L -H "Content-Type: application/json" -X POST "http://${mesos_master}:4400/scheduler/iso8601" -d@${i} &> /dev/null 
 done
-
